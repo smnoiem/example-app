3645d8e1ec5833566ee74a5d17e728d118c5725f
Noiem Uddin <smnoiem@gmail.com>
2022-03-27 18:37:28 +0600

    User resource controller and route created, ajax request written

--- public/assets/js/new-user-form.js
+++ public/assets/js/new-user-form.js
@@ -62,16 +62,31 @@ $( document ).ready(function() {
 
   $('#new-user-form').on('submit', function(event) {
     event.preventDefault();
+    var csrfToken = $("input[name=_token]").val();
     var name = $("#username").val();
     var email = $("#email").val();
     var password = $("#password").val();
     var confirmPassword = $("#confirmPassword").val();
     var address = $("#address").val();
     var role = $("#role").val();
-    
+
     if(name && email && password && confirmPassword && address && role && password==confirmPassword) 
     {
       //register through an ajax request
+      //console.log(csrfToken);
+      $.ajax({
+        async: false,
+        url: "/users",
+        type: "POST",
+        data: {
+            _token: csrfToken,
+            name: name,
+        },
+        cache: false,
+        success: function(response){
+          console.log(response);
+        }
+      });
     }
     
   });
